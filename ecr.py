9ae73feb9463ccee2a54c566e4c0128ef1bfe7cf
kannan <137377180+kannan742003@users.noreply.github.com>
2023-10-30 15:01:27 +0530

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -1,9 +1,8 @@
 import boto3
 
-ecr_client = boto3.client('ecr')
-
-repository_name = "my_monitoring_app_image"
+ecr_client = boto3.client('ecr', region_name='ap-south-1')  
+repository_name = "my_cloud_native_app_image"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
-repository_uri = response ['repository']['repositoryUri']
+repository_uri = response['repository']['repositoryUri']
 print(repository_uri)
